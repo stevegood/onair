46eb936c4422b65a1cc7c9bd5bafe5d82907b0f6
Steve Good <steve@stevegood.org>
2015-04-09 22:23:32 -0500

    Added gpio functionality

--- services/Lamp.js
+++ services/Lamp.js
@@ -1,14 +1,34 @@
-var monitor = require('./StreamMonitor');
+var monitor = require('./StreamMonitor'), gpio;
+
+try {
+  gpio = require('pi-gpio');
+} catch (e) {
+  console.error(e);
+}
 
 function Lamp() {
 
-  var online = false;
+  var online = false,
+      pins;
+
+  function setPinsTo(value) {
+    if (gpio) {
+      for(var i=0; i < pins.length; i++) {
+        gpio.open(pins[i], "output", function(err) {
+          gpio.write(pins[i], value || 1, function() {
+            gpio.close(pins[i]);
+          });
+        });
+      }
+    }
+  }
 
   function onlineHandler() {
     // turn the lamp on
     if (!online) {
       online = true;
       console.log('Turning the lamp ON');
+      setPinsTo(0);
     }
   }
 
@@ -17,6 +37,7 @@ function Lamp() {
     if (online) {
       online = false;
       console.log('Turning the lamp OFF');
+      setPinsTo(1);
     }
   }
 
@@ -25,8 +46,14 @@ function Lamp() {
       var streamName = process.env.STREAM_NAME,
           _interval = process.env.INTERVAL || 30;
 
+      pins = process.env.PINS.split(',');
+
       console.log('Starting up the lamp!');
       console.log('Watching for status changes on ' + streamName + ' every ' + _interval + ' seconds');;
+      console.log('Will manage pins ' + pins);
+      console.log('Setting pins high to start');
+
+      setPinsTo(1);
 
       if (streamName && _interval) {
         monitor.watch(streamName, (_interval * 1000), onlineHandler, offlineHandler);
